bb0ee93e804c130e07920e13c28d157c9749bc42
Mateusz Gostański <mg@grixu.dev>
2021-02-12 13:15:43 +0100

    Removed old files (whoooa)

--- src/Operator/DataTransferObjects/OperatorRoleData.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Grixu\SociusClient\Operator\DataTransferObjects;
-
-class OperatorRoleData extends \Grixu\SociusModels\Operator\DataTransferObjects\OperatorRoleData
-{
-    public int $id;
-    public ?OperatorDataCollection $operators;
-}
--- src/Product/DataTransferObjects/BrandData.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Grixu\SociusClient\Product\DataTransferObjects;
-
-class BrandData extends \Grixu\SociusModels\Product\DataTransferObjects\BrandData
-{
-    public int $id;
-    public ?ProductDataCollection $products;
-}
