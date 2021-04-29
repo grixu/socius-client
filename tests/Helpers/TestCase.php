93cdf158bb00c95cd76d1c22dd1966e3442f0395
Mateusz Gostanski <mg@grixu.dev>
2021-04-29 15:26:56 +0200

    Created TestCase

---
+++ tests/Helpers/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Grixu\SociusClient\Tests\Helpers;
+
+use Grixu\ApiClient\ApiClientServiceProvider;
+use Grixu\SociusClient\SociusClientServiceProvider;
+use Grixu\SociusModels\SociusModelsServiceProvider;
+use Grixu\Synchronizer\SynchronizerServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ApiClientServiceProvider::class,
+            SynchronizerServiceProvider::class,
+            SociusClientServiceProvider::class,
+            SociusModelsServiceProvider::class
+        ];
+    }
+}
