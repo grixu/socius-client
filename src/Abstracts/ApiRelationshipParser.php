50567721ab2f19c92e14576ca3ef56a08ad6eebc
Mateusz Gostanski <mg@grixu.dev>
2021-04-30 16:10:40 +0200

    Created ApiRelationshipParser

---
+++ src/Abstracts/ApiRelationshipParser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Grixu\SociusClient\Abstracts;
+
+use Grixu\ApiClient\Data\StraightKeyParser;
+use Grixu\Synchronizer\Contracts\ParserInterface;
+use Illuminate\Support\Collection;
+
+abstract class ApiRelationshipParser implements ParserInterface
+{
+    private StraightKeyParser $parser;
+
+    public function __construct(string $dtoClass)
+    {
+        $this->parser = new StraightKeyParser($dtoClass);
+    }
+
+    public function parse(Collection $collection): Collection
+    {
+        return $this->parser->parse($collection->flatten(2));
+    }
+}
